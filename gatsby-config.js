8f609e452a37e9d0df41298f841511177e7a6256
Marcos <vmarcosp.pereira@gmail.com>
2020-01-04 18:07:12 -0300

    feat(Post): Adding an introduction post

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     siteUrl: `https://blog.blumenaujs.org`,
     description: `Comunidade JavaScript de Blumenau e Região`,
     hero: {
-      heading: `Heading BlumenauJS`,
+      heading: `BlumenauJS - Comunidade de JavaScript de Blumenau`,
       maxWidth: 652,
     },
     social: [
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,10 @@
+
+import novelaTheme from '@narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui'
+
+export default {
+  ...novelaTheme,
+  colors: {
+    ...novelaTheme.colors,
+    primary: '#515050',
+  }
+};
